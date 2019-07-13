34663be42aa86d70d76397d478722068f574621e
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-07-13 11:28:46 +0800

    add not found page

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Malcolm Kee's Gatsby Site</h1>
+      <p>crafted with tears and love.</p>
+    </div>
+  );
+};
+
+export default HomePage;
